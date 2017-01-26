a1824fa0b67e4ab6a25fa9ea2a828abf7bdd9a10
aliff.macapinlac <aliff.macapinlac@gmail.com>
2017-01-26 09:53:21 -0800

    updating

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,39 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("JavaScript has taken control");
+		$("button#testjs").text("Often times I feel apathetic and Helpless :_ (");
+		$(".jumbotron p").addClass("active");
+
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	var containingProject = $("a.thumbnail").closest(".project");
+
+
+	
+	/*function projectClick(e){
+		e.preventDefault();;
+		$(this).append("<div class='project-description'><p>project-description of the project.</p></div>");
+	}
+	containingProject.click(projectClick);
+*/
+	function projectClick(e){
+	e.preventDefault();
+ 
+ 	if($(".project-description").length == 0){
+		$(this).append("<div class='project-description'><p>project-description of the project.</p></div>");
+		$(this).css("background-color", "#7fff00");
+		
+
+	}else{
+		$(".project-description").html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+	}
+
+	}
+	//$("a.thumbnail").click(projectClick);
+	containingProject.click(projectClick);
+
+
 }
